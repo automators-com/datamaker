afaf46306223ac0f3c3a9fddd65145f6c461043e
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-10-21 19:19:24 +0200

    Remove unused import

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Header from "../header";
 import Sidebar from "../sidebar";
-// import Sidebar from "../sidebar";
 
 type defaultLayoutProps = {
   children: React.ReactNode;
