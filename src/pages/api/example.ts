04b47a36b30bde3d60f173a921d1bc6386918bfa
aminitoy <amin.chirazi@gmail.com>
2022-10-24 14:43:33 +0200

    Changing Examples Message

--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const example = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: "Hello Datamaker" });
+  res.status(200).json({ message: "Hello Datamaker!" });
 };
 
 export default example;
