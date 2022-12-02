41a3c96998bc678122f4ddfc3dbefb8b7b189eae
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-03 00:01:09 +0200

    Update tailwind content

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import "../app/globals.css";
+
+export default function LandingPage() {
+  return (
+    <div className=" bg-black ">
+      <div className="w-30 bg-red-100">Hello</div>
+      <div>Hello</div>
+      <div className="bg-primary">Hello</div>
+    </div>
+  );
+}
