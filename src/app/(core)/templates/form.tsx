9024c066f75918c56dbccb8a065e197fd237c119
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-13 23:00:13 +0200

    Eslint disable exhaustive deps

--- src/app/(core)/templates/form.tsx
+++ src/app/(core)/templates/form.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
