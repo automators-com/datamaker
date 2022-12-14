88bab62d4ceaf9c9c3027599b8aa61a60b2fb9e3
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-14 23:42:03 +0200

    Added loader

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { NavContext } from "../../components/context/context";
 import Header from "../../components/header";
 import Sidebar from "../../components/sidebar";
 import "../globals.css";
+import MoonLoader from "../../components/loaders/moonLoader";
 
 export default function CoreLayout({
   children,
@@ -13,25 +16,38 @@ export default function CoreLayout({
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
   const [theme, setTheme] = useState<string>("root");
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  return (
-    <NavContext.Provider value={{ isNavOpen, setIsNavOpen }}>
-      <Header
-        theme={theme}
-        setTheme={setTheme}
-        setSidebarOpen={setSidebarOpen}
-      />
-      <div className="flex flex-row">
-        <Sidebar
-          open={isNavOpen}
-          setOpen={setIsNavOpen}
-          sidebarOpen={sidebarOpen}
+
+  if (status === "loading"){
+    return <MoonLoader />;
+  }
+
+
+  if (!session) {
+    // redirect to sign in page if not signed in
+    router.push("/signin");
+  } else if (session && session.user) {
+    return (
+      <NavContext.Provider value={{ isNavOpen, setIsNavOpen }}>
+        <Header
+          theme={theme}
+          setTheme={setTheme}
           setSidebarOpen={setSidebarOpen}
         />
-        <main className="h-[calc(100vh_-_5rem)] max-h-[calc(100vh_-_5rem)] w-full overflow-auto">
-          {children}
-        </main>
-      </div>
-    </NavContext.Provider>
-  );
+        <div className="flex flex-row">
+          <Sidebar
+            open={isNavOpen}
+            setOpen={setIsNavOpen}
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+          />
+          <main className="h-[calc(100vh_-_5rem)] max-h-[calc(100vh_-_5rem)] w-full overflow-auto">
+            {children}
+          </main>
+        </div>
+      </NavContext.Provider>
+    );
+  }
 }
