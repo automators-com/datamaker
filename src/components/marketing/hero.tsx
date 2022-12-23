93e938c5c9a01ee45ad18c16101effa4c0598d75
aminitoy <amin.chirazi@gmail.com>
2022-12-23 15:03:55 +0100

    Repairing hero section for mobile

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -1,11 +1,27 @@
+// export default function Hero() {
+//   return (
+//     <section className="relative -z-10 flex h-[90vh] flex-col items-center justify-start overflow-visible bg-gradient-to-r from-[#459CA7] to-[#482B7C]">
+//       <div className="mx-5 mt-[10em] flex flex-col items-center text-center">
+//         <p className="mb-4 mt-20 text-4xl md:mb-0">
+//           <strong>Fake it. While you make it.</strong>
+//         </p>
+//         <p className="pt-2 text-xl">
+//           With our intuitive point-and-click interface, creating high-quality
+//           synthetic data has never been easier.
+//         </p>
+//       </div>
+//     </section>
+//   );
+// }
+
 export default function Hero() {
   return (
     <section className="relative -z-10 flex h-[90vh] flex-col items-center justify-start overflow-visible bg-gradient-to-r from-[#459CA7] to-[#482B7C]">
-      <div className="mx-5 mt-[10em] flex flex-col items-center text-center">
-        <p className="mb-4 mt-20 text-4xl md:mb-0">
+      <div className="flex flex-col items-center text-center md:mx-5 md:mt-10">
+        <p className="mb-4 mt-48 text-lg md:mb-0 md:text-4xl">
           <strong>Fake it. While you make it.</strong>
         </p>
-        <p className="pt-2 text-xl">
+        <p className="pt-2 text-lg md:text-xl">
           With our intuitive point-and-click interface, creating high-quality
           synthetic data has never been easier.
         </p>
