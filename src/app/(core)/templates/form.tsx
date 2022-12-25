15e11cedeb6471271e5fe14180e214681b67479a
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-25 22:34:27 +0200

    Move form menu into it's own component

--- src/app/(core)/templates/form.tsx
+++ src/app/(core)/templates/form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 "use client";
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
@@ -8,11 +9,9 @@
 import {
   ArrowUturnDownIcon,
   CheckIcon,
-  EllipsisVerticalIcon,
   PlusCircleIcon,
-  TrashIcon,
 } from "@heroicons/react/24/outline";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { Dispatch, SetStateAction } from "react";
 import type { TemplateForm } from "./types";
 import type { Template } from "./types";
@@ -21,8 +20,7 @@ import { Input } from "../../../components/input";
 import CollapsedContainer from "./collapsedContainer";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useFieldArray, useFormContext } from "react-hook-form";
-import { Menu, Transition } from "@headlessui/react";
-import { classNames } from "../../../utilities/className";
+import FormMenu from "./formMenu";
 
 export default function Form({
   selectedTemplate,
@@ -134,50 +132,12 @@ export default function Form({
             <CheckIcon />
             Save
           </button>
-          <Menu as="div" className={`relative inline-block h-full text-left`}>
-            <Menu.Button className="flex items-center text-base-content focus:outline-none">
-              <EllipsisVerticalIcon className="h-5 w-8" aria-hidden="true" />
-            </Menu.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right overflow-auto rounded-md bg-base-100 shadow-lg ring-1 ring-secondary ring-opacity-5 focus:outline-none">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={classNames(
-                        active
-                          ? "bg-base-200 bg-opacity-50 text-base-content"
-                          : "text-base-content",
-                        "group flex items-center px-4 py-2 text-sm hover:cursor-pointer"
-                      )}
-                      onClick={() => {
-                        if (selectedTemplate)
-                          deleteMutation.mutate(selectedTemplate);
-                        setIsFormOpen(false);
-                        setSelectedTemplate(null);
-                      }}
-                    >
-                      <span
-                        className="mr-3 h-5 w-5 text-accent"
-                        aria-hidden="true"
-                      >
-                        <TrashIcon className="text-error" />
-                      </span>
-                      Delete Template
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+          <FormMenu
+            selectedTemplate={selectedTemplate}
+            deleteMutation={deleteMutation}
+            setIsFormOpen={setIsFormOpen}
+            setSelectedTemplate={setSelectedTemplate}
+          />
         </div>
       </div>
 
@@ -188,7 +148,7 @@ export default function Form({
           })}
           error={isSubmit ? errors.templateName?.message : ""}
           id={"templateName"}
-          placeholder="name"
+          placeholder="Template Name"
           type="text"
           label="Template Name"
         />
---
+++ src/app/(core)/templates/formMenu.tsx
@@ -0,0 +1,62 @@
+import { Fragment } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import type { UseMutationResult } from "@tanstack/react-query";
+import { Menu, Transition } from "@headlessui/react";
+import { classNames } from "../../../utilities/className";
+import { EllipsisVerticalIcon, TrashIcon } from "@heroicons/react/24/outline";
+import type { Template } from "./types";
+
+export default function FormMenu({
+  selectedTemplate,
+  deleteMutation,
+  setIsFormOpen,
+  setSelectedTemplate,
+}: {
+  selectedTemplate: Template | null;
+  deleteMutation: UseMutationResult<Template[], Error, Template, unknown>;
+  setIsFormOpen: Dispatch<SetStateAction<boolean>>;
+  setSelectedTemplate: Dispatch<SetStateAction<Template | null>>;
+}): JSX.Element {
+  return (
+    <Menu as="div" className={`relative inline-block h-full text-left`}>
+      <Menu.Button className="flex items-center text-base-content focus:outline-none">
+        <EllipsisVerticalIcon className="h-5 w-8" aria-hidden="true" />
+      </Menu.Button>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-4 w-48 origin-top-right overflow-auto rounded-md bg-base-100 shadow-lg ring-1 ring-secondary ring-opacity-5 focus:outline-none">
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={classNames(
+                  active
+                    ? "bg-base-200 bg-opacity-50 text-base-content"
+                    : "text-base-content",
+                  "group flex items-center px-4 py-2 text-sm hover:cursor-pointer"
+                )}
+                onClick={() => {
+                  if (selectedTemplate) deleteMutation.mutate(selectedTemplate);
+                  setIsFormOpen(false);
+                  setSelectedTemplate(null);
+                }}
+              >
+                <span className="mr-3 h-5 w-5 text-accent" aria-hidden="true">
+                  <TrashIcon className="text-error" />
+                </span>
+                Delete Template
+              </a>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
