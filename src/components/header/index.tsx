50e20bd93bd79733de4a9ea404516034e299513f
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-26 13:12:18 +0200

    Added sign out dropdown

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,8 @@
 import { Menu, Transition } from "@headlessui/react";
 import { signOut } from "next-auth/react";
 import {
+  ArrowLeftOnRectangleIcon,
+  EllipsisVerticalIcon,
   Bars3BottomLeftIcon,
   FireIcon,
   MoonIcon,
@@ -58,14 +60,50 @@ export default function Header({
               }
             })}
           </Menu.Button>
-          <button
-            className="btn-primary ml-8 h-10 w-28 rounded-md px-4 py-2"
-            onClick={() => {
-              signOut().catch((err) => console.log(err));
-            }}
+
+          <Menu
+            as="div"
+            className={`relative inline-block h-full px-4 text-left`}
           >
-            Sign Out
-          </button>
+            <Menu.Button className="flex h-10 items-center text-primary-content focus:outline-none">
+              <EllipsisVerticalIcon
+                className="h-5 w-8 text-primary-content"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right overflow-auto rounded-md bg-base-100 shadow-lg ring-1 ring-secondary ring-opacity-5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      onClick={() => {
+                        signOut({ callbackUrl: "/" }).catch((err) =>
+                          console.log(err)
+                        );
+                      }}
+                      className={classNames(
+                        active
+                          ? "bg-accent text-accent-content"
+                          : "text-base-content",
+                        "group flex cursor-pointer items-center px-2 py-2 text-sm "
+                      )}
+                    >
+                      <ArrowLeftOnRectangleIcon className="h-5 w-5" />
+                      <p className="ml-2">Sign Out</p>
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
 
         <Transition
