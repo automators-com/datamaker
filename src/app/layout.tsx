3a0571852c1404a6289b05c85d3dc74dcb438f0f
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-26 12:18:17 +0200

    Added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -15,6 +16,23 @@ export default function RootLayout({
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         <html lang="en">
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-8G44GWPK1S"
+            strategy="lazyOnload"
+          />
+          <Script
+            id="gtag"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-8G44GWPK1S');
+            `,
+            }}
+          />
+
           <head />
           {children}
         </html>
