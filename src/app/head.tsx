c6ddd22da20a76a2991b0379f5cb9a5a340eb196
aminitoy <amin.chirazi@gmail.com>
2022-12-27 22:40:34 +0100

    Adding SEO meta tags

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,10 +3,32 @@ export default function Head() {
     <>
       <title>Datamaker</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Effortlessly generate quality test data with datamaker.app" />
+      <meta
+        name="description"
+        content="Effortlessly generate quality test data with datamaker.app"
+      />
       <meta property="og:image" content="/assets/og-image.png" />
       <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:url" content="https://datamaker.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Datamaker" />
+      <meta
+        property="og:description"
+        content="Effortlessly generate quality test data with datamaker.app"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="datamaker.app" />
+      <meta property="twitter:url" content="https://datamaker.app" />
+      <meta name="twitter:title" content="Datamaker" />
+      <meta
+        name="twitter:description"
+        content="Effortlessly generate quality test data with datamaker.app"
+      />
+      <meta name="twitter:image" content="/assets/og-image.png" />
     </>
   );
 }
