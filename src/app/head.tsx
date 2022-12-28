1c6614810ce27af0607620154b7e3996bd6f0a91
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-12-28 03:28:20 +0200

    Use gh hosted og images

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,4 @@
 export default function Head() {
-  // get the current environment
-  const env = process.env.NODE_ENV;
-
-  // set the domain based on the environment
-  let domain = "https://datamaker.app";
-  if (env !== "production") {
-    domain = "https://dev.datamaker.app";
-  }
-
   return (
     <>
       <title>Datamaker</title>
@@ -16,12 +7,15 @@ export default function Head() {
         name="description"
         content="Effortlessly generate quality test data with datamaker.app"
       />
-      <meta property="og:image" content={`${domain}/assets/og-image.png`} />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/automators-com/datamaker/dev/public/assets/og-image.png"
+      />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <link rel="icon" href="/favicon.ico" />
 
-      <meta property="og:url" content={`${domain}`} />
+      <meta property="og:url" content="https://datamaker.app" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Datamaker" />
       <meta
@@ -31,13 +25,16 @@ export default function Head() {
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="datamaker.app" />
-      <meta property="twitter:url" content={`${domain}`} />
+      <meta property="twitter:url" content="https://datamaker.app" />
       <meta name="twitter:title" content="Datamaker" />
       <meta
         name="twitter:description"
         content="Effortlessly generate quality test data with datamaker.app"
       />
-      <meta name="twitter:image" content={`${domain}/assets/og-image.png`} />
+      <meta
+        name="twitter:image"
+        content="https://raw.githubusercontent.com/automators-com/datamaker/dev/public/assets/og-image.png"
+      />
     </>
   );
 }
