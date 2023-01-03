0ffc471756ecf9d9784da04b1a1708c2880d86b2
asmaa_salem <a.asmaasalem@gmail.com>
2023-01-03 21:29:36 +0200

    Map datatypes with value

--- src/utilities/tableData.ts
+++ src/utilities/tableData.ts
@@ -12,25 +12,137 @@ export const getTableData = (len: number, data: TemplateField[]): any[] => {
         ? field.constraints.filter((cons) => cons.name?.id === 2)[0].value!
         : 20;
 
-      const type = field.dataType?.id ? field.dataType.id : field.dataType;
+      const type =  field.dataType.id ;
       // if(type === 2 && min > max) max = min+1
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      o[field.fieldName] =
-        type === 2
-          ? faker.datatype.number({
+
+      const getValue = (type: number) => {
+        switch(type){
+          case 2:
+            return faker.datatype.number({
               min: min,
               max: max > min ? max : undefined,
             })
-          : // : type === 6
-          // ? faker.datatype.array(min)
-          type === 7
-          ? faker.datatype.boolean()
-          : type === 3
-          ? faker.name.fullName()
-          : type === 4
-          ? faker.internet.email()
-          : faker.datatype.string(max);
+            break;
+          case 3: 
+            return faker.name.fullName()
+            break;
+          case 4: 
+            return faker.internet.email()
+            break;
+          case 5: 
+            return faker.address.streetAddress()
+            break;
+          case 6: 
+            return faker.address.city()
+            break;
+          case 7: 
+            return faker.address.country()
+            break;
+          case 8: 
+            return faker.phone.number()
+            break;
+          case 9:  
+            return faker.address.zipCode()
+            break;
+          case 10: 
+            return faker.name.gender()
+            break;
+          case 11: 
+            return faker.image.avatar()
+            break;
+          case 12: 
+            return faker.name.jobTitle()
+            break;
+          case 13: 
+            return faker.finance.accountName()
+            break;
+          case 14: 
+            return faker.finance.iban()
+            break;
+          case 15: 
+            return faker.finance.currencyName();
+            break;
+          case 16: 
+            return faker.finance.creditCardNumber()
+            break;
+          case 17: 
+            return faker.finance.account()
+            break;
+          case 18: 
+            return faker.finance.amount()
+            break;
+          case 19: 
+            return faker.internet.password()
+            break;
+          case 20: 
+            return faker.internet.domainName()
+            break;
+          case 21: 
+            return faker.color.rgb()
+            break;
+          case 22: 
+            return faker.internet.emoji()
+            break;
+          case 23: 
+            return faker.internet.ipv4()
+            break;
+          case 24: 
+            return faker.internet.mac()
+            break;
+          case 25: 
+            return faker.internet.url()
+            break;
+          case 26: 
+            return faker.commerce.product()
+            break;
+          case 27: 
+            return faker.commerce.department()
+            break;
+          case 28: 
+            return faker.commerce.price()
+            break;
+          case 29: 
+            return faker.commerce.productName()
+            break;
+          case 30: 
+            return faker.datatype.boolean()
+            break;
+          case 31: 
+            return faker.date.birthdate()
+            break;
+          case 32: 
+            return 
+            break;
+          case 33: 
+            return faker.date.month()
+            break;
+          case 34: 
+            return faker.date.weekday()
+            break;
+          case 35: 
+            return faker.address.timeZone()
+            break;
+          default:
+            return faker.datatype.string(max)
+        }
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      o[field.fieldName] = getValue(type);
+        // type === 2
+        //   ? faker.datatype.number({
+        //       min: min,
+        //       max: max > min ? max : undefined,
+        //     })
+        //   : // : type === 6
+        //   // ? faker.datatype.array(min)
+        //   type === 7
+        //   ? faker.datatype.boolean()
+        //   : type === 3
+        //   ? faker.name.fullName()
+        //   : type === 4
+        //   ? faker.internet.email()
+        //   : faker.datatype.string(max);
     });
 
     return o;
