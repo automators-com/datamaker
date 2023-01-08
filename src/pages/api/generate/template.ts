770a8df9e82d53ea52038bf52d4d22c557f1d2d9
Ratul Maharaj <ratulmaharaj@gmail.com>
2023-01-09 01:01:47 +0200

    Use AI to generate template

--- src/pages/api/generate/template.ts
+++ src/pages/api/generate/template.ts
@@ -1,6 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import type { NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "../auth/[...nextauth]";
 import { unstable_getServerSession as getServerSession } from "next-auth/next";
+import csv from "csvtojson";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,13 +29,61 @@ export default async function handler(
   // TODO: Check to see if the user is allowed to access this endpoint
   // TODO: For now all users will be allowed to
 
-  console.log(req.body);
-  console.log(fakerTypes);
+  const data = req.body;
 
-  //   wait for 30 seconds
-  await new Promise((resolve) => setTimeout(resolve, 30000));
+  const jsonData = await csv()
+    .fromString(data)
+    .then((jsonObj) => {
+      return jsonObj;
+    });
 
-  return res.status(200).json({ message: "Hello World" });
+  const template: Array<{
+    fieldName: string;
+    dataType: { id: number; name: string };
+    constraints: [];
+  }> = [];
+
+  if (jsonData.length > 0) {
+    const columns = Object.keys(jsonData[0]);
+    for (const column of columns) {
+      const rows = JSON.stringify(jsonData.map((item) => item[column]));
+      const index = columns.indexOf(column);
+      const prompt = `Classify the following data ${rows} with label ${column} into one of the following ${fakerTypes}.`;
+      await openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt: prompt,
+          max_tokens: 256,
+        })
+        .then((res) => {
+          if (res.data.choices[0].text) {
+            // check if the type is valid
+            if (!fakerTypes.includes(res.data.choices[0].text.trim())) {
+              console.log(
+                res.data.choices[0].text.trim(),
+                "is an Invalid type for",
+                column
+              );
+              template.push({
+                fieldName: column,
+                dataType: { id: index, name: "String" },
+                constraints: [],
+              });
+            }
+            template.push({
+              fieldName: column,
+              dataType: { id: index, name: res.data.choices[0].text?.trim() },
+              constraints: [],
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  return res.status(200).json({ template });
 }
 
 // Our predefined datatypes - faker.js
@@ -63,4 +123,5 @@ const fakerTypes = [
   "Month",
   "Weekday",
   "Time Zone",
+  "String",
 ];
